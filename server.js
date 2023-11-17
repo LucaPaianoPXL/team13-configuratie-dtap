ffcd8b287b413f3d54faba1f4c1e71e532b62fd4
LucaPaianoPXL <145565041+LucaPaianoPXL@users.noreply.github.com>
2023-11-17 19:13:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ console.log(process.env.PORT_PROD); // Log PORT_PROD value
 
 const port = process.env.NODE_ENV === 'test' ? process.env.PORT_TEST : process.env.PORT_PROD;
 console.log(process.env.NODE_ENV);
-//const port = process.env.NODE_ENV === 'test' ? process.env.PORT_TEST : process.env.PORT_PROD;
-//console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-//app.listen(port, () => {    console.log(`Server is running on port ${port}`);});
+
+app.listen(port);
+console.log("Server running on port: " + port);
