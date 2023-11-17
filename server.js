18b1f04ad40831ec52a2fd3327f32dd1219af2dc
LucaPaianoPXL <145565041+LucaPaianoPXL@users.noreply.github.com>
2023-11-17 08:11:41 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+let port;
+if(process.env.NODE_ENV === 'testServer'){
+  port = process.env.PORT_TEST || 3000;
+}else if(process.env.NODE_ENV === 'productionServer'){
+  port = process.env.PORT_PROD || 3000;
+}
+
+//const app = require("./app");
+//const port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log("Server running on port: " + port);
