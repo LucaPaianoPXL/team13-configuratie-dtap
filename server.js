dc0b80d9208f55c7b64d95cfeb76e68aab794813
LucaPaianoPXL <145565041+LucaPaianoPXL@users.noreply.github.com>
2023-11-17 15:50:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.NODE_ENV === 'test' ? process.env.PORT_TEST : process.env.PORT_PROD;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
