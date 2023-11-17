44e4ba59ba8d085189247bb512689df015396796
LucaPaianoPXL <145565041+LucaPaianoPXL@users.noreply.github.com>
2023-11-17 19:18:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-require('dotenv').config();
 const app = require("./app");
+const port = process.env.PORT || 3000;
 
-console.log(process.env.PORT_TEST); // Log PORT_TEST value
-console.log(process.env.PORT_PROD); // Log PORT_PROD value
-
-const port = process.env.NODE_ENV === 'test' ? process.env.PORT_TEST : process.env.PORT_PROD;
-console.log(process.env.NODE_ENV);
-
-app.listen(${port});
-console.log("Server running on port: " + ${port});
+app.listen(port);
+console.log("Server running on port: " + port);
